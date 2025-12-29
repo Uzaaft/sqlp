afafff3d3eab4d311cb3ee80d3caceac2a7e3530
Uzair Aftab <git@uzaaft.me>
2025-12-30 00:13:01 +0100

    chore: update public API exports

--- src/sqlp/__init__.py
+++ src/sqlp/__init__.py
@@ -2,9 +2,17 @@
 
 __version__ = "0.1.0"
 
-# Public API - populate as implementation progresses
+from sqlp.table import Table
+from sqlp.types import Column
+from sqlp.pool import AsyncPool
+from sqlp.schema import validate_schema
+from sqlp.executor import ExecutableQuery, ExecutableMutation
+
 __all__ = [
     "Table",
     "Column",
     "AsyncPool",
+    "validate_schema",
+    "ExecutableQuery",
+    "ExecutableMutation",
 ]
